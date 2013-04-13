ef1a7a18a2fc5a5c5a43d72cbc976f5467d312ae
Patrick H. <iamstillawesome1@gmail.com>
2013-04-13 16:41:40 -0400

    Finished about activity

--- gen/edu/mdc/entec/R.java
+++ gen/edu/mdc/entec/R.java
@@ -64,43 +64,51 @@ public final class R {
         public static final int wolfson=0x7f02002a;
     }
     public static final class id {
-        public static final int btnAngel=0x7f080005;
-        public static final int btnCourses=0x7f080008;
-        public static final int btnEmail=0x7f080002;
-        public static final int btnEvents=0x7f080003;
-        public static final int btnHome=0x7f08000b;
-        public static final int btnLibrary=0x7f080007;
-        public static final int btnMap=0x7f080004;
-        public static final int btnMyMdc=0x7f080001;
-        public static final int btnNews=0x7f080006;
-        public static final int btnPhoneDir=0x7f08000a;
-        public static final int btnPublicSafety=0x7f080009;
-        public static final int button1=0x7f080021;
-        public static final int id_list_view=0x7f080000;
-        public static final int id_list_view_hialeah=0x7f080011;
-        public static final int id_list_view_west=0x7f08001c;
-        public static final int imageView1=0x7f08001d;
-        public static final int imgAssociation=0x7f080020;
-        public static final int imgCampus=0x7f08000c;
-        public static final int imgCreatedBy=0x7f08001f;
-        public static final int imgHialeah=0x7f080014;
-        public static final int imgHialeahMap=0x7f08000f;
-        public static final int imgHomestead=0x7f080015;
-        public static final int imgInformation=0x7f080013;
-        public static final int imgInteramerican=0x7f080016;
-        public static final int imgKendall=0x7f080017;
-        public static final int imgMdcLogo=0x7f080012;
-        public static final int imgMedical=0x7f080018;
-        public static final int imgNorth=0x7f080019;
-        public static final int imgPhone=0x7f080022;
-        public static final int imgSupport=0x7f08001e;
-        public static final int imgWest=0x7f08001a;
-        public static final int imgWolfson=0x7f08001b;
-        public static final int menu_settings=0x7f080024;
-        public static final int safetyContacts=0x7f080023;
-        public static final int textView1=0x7f08000d;
-        public static final int txtHialeahMap=0x7f08000e;
-        public static final int txtPhoneDirectory=0x7f080010;
+        public static final int btnAngel=0x7f08000c;
+        public static final int btnCourses=0x7f08000f;
+        public static final int btnEmail=0x7f080009;
+        public static final int btnEvents=0x7f08000a;
+        public static final int btnHome=0x7f080012;
+        public static final int btnLibrary=0x7f08000e;
+        public static final int btnMap=0x7f08000b;
+        public static final int btnMyMdc=0x7f080008;
+        public static final int btnNews=0x7f08000d;
+        public static final int btnPhoneDir=0x7f080011;
+        public static final int btnPublicSafety=0x7f080010;
+        public static final int button1=0x7f080029;
+        public static final int id_list_view=0x7f080023;
+        public static final int id_list_view_hialeah=0x7f080018;
+        public static final int id_list_view_west=0x7f080024;
+        public static final int imageView1=0x7f080025;
+        public static final int imgAssociation=0x7f080028;
+        public static final int imgCampus=0x7f080013;
+        public static final int imgCreatedBy=0x7f080027;
+        public static final int imgHialeah=0x7f08001b;
+        public static final int imgHialeahMap=0x7f080016;
+        public static final int imgHomestead=0x7f08001c;
+        public static final int imgInformation=0x7f08001a;
+        public static final int imgInteramerican=0x7f08001d;
+        public static final int imgKendall=0x7f08001e;
+        public static final int imgMdcLogo=0x7f080019;
+        public static final int imgMedical=0x7f08001f;
+        public static final int imgNorth=0x7f080020;
+        public static final int imgPhone=0x7f08002a;
+        public static final int imgSupport=0x7f080026;
+        public static final int imgWest=0x7f080021;
+        public static final int imgWolfson=0x7f080022;
+        public static final int menu_settings=0x7f08002c;
+        public static final int safetyContacts=0x7f08002b;
+        public static final int textView1=0x7f080014;
+        public static final int txtHialeahMap=0x7f080015;
+        public static final int txtMTC=0x7f080000;
+        public static final int txtPhoneDirectory=0x7f080017;
+        public static final int txt_about1=0x7f080007;
+        public static final int txt_about2=0x7f080006;
+        public static final int txt_about3=0x7f080005;
+        public static final int txt_about4=0x7f080004;
+        public static final int txt_about5=0x7f080003;
+        public static final int txt_about6=0x7f080002;
+        public static final int txt_about7=0x7f080001;
     }
     public static final class layout {
         public static final int about_us_layout=0x7f030000;
@@ -142,13 +150,22 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int build_number=0x7f040009;
+        public static final int frank_gonzalez=0x7f04000c;
         /** Google Analytics tracking ID
          */
         public static final int ga_trackingId=0x7f040000;
         public static final int hello_world=0x7f040002;
         public static final int information=0x7f040007;
+        public static final int jack_lusby=0x7f04000a;
+        public static final int joel_cruz=0x7f04000b;
+        public static final int joeseph_george=0x7f04000d;
+        public static final int luis_portillo=0x7f04000f;
+        public static final int mario_thomas=0x7f040010;
         public static final int mdc=0x7f040005;
+        public static final int mdc_mobiletechclub=0x7f040008;
         public static final int menu_settings=0x7f040003;
+        public static final int patrick_helmus=0x7f04000e;
         public static final int select_to_call=0x7f040006;
         public static final int title_activity_home=0x7f040004;
     }
--- src/edu/mdc/entec/AboutUs.java
+++ src/edu/mdc/entec/AboutUs.java
@@ -4,104 +4,13 @@ import com.google.analytics.tracking.android.EasyTracker;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class AboutUs extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
         
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about_us_layout);
-        
-        String[] contributors={"Wolfson Campus Entec Dept.",
-        		"Professor Jack Lusby",
-        		"Mobile Technology Club",
-        		"Joel Cruz",
-        		"Frank Gonzales",
-        		"Joseph George",
-        		"Patrick Helmus",
-        		"Luis Protillo",
-        		"Mario Thomas"
-        };
-        
-        ListView lvcontrib = new ListView(this);
-        
-        lvcontrib.setAdapter(new ArrayAdapter<String>(this,
-        		android.R.layout.simple_list_item_1,contributors));
-        
-        
-        setContentView(lvcontrib);
-       
-    /*   
-        lvcontrib.setOnItemClickListener(mListViewClicks);
-	}
-        
-       private OnItemClickListener mListViewClicks = new OnItemClickListener() {
-
-    	   @Override
-   		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-   				long arg3) {
-   			// TODO Auto-generated method stub
-		        		
-		        		switch(arg2){
-		        		case 0:
-		        			//Operator:  305-237-8000 
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("tel:305-237-8000")));
-		        			break;
-		        		case 1:
-		        			//Reception Desk: 305-237-8918
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("tel:305-237-8918")));
-		        			break;
-		        		case 2:
-		        			//Admissions:  305-237-8900
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("tel:305-237-8900")));
-		        			break;
-		        		case 3:
-		        			//Advisement:  305-237-8947
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("tel:305-237-8947")));
-		        			break;
-		        		case 4:
-		        			//Bursar: 305-237-8951
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("tel:305-237-8951")));
-		        			break;
-		        		case 5:
-		        			//Campus Services: 305-237-8970
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("tel:305-237-8970")));
-		        			break;
-		        		case 6:
-		        			//Financial Aid: 305-237-8941
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("tel:305-237-8941")));
-		        			break;
-		        		case 7:
-		        			//Library: 305-237-8931
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("tel:305-237-8931")));
-		        			break;
-		        		case 8:
-		        			//Student Life: 305-237-8904
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("305-237-8904")));
-		        			break;
-		        		case 9:
-		        			//Testing:  305-237-8979
-		        			startActivity(new Intent(Intent.ACTION_DIAL,
-		        					Uri.parse("305-237-8979")));
-		        			break;
-		        		case 10:
-		        			startActivity(new Intent(Intent.ACTION_VIEW, 
-		        					Uri.parse("http://www.mdc.edu/west/West%20Campus%20Directory.asp")));
-		        			break;
-		        					}
-						}
-				*/	
+		setContentView(R.layout.about_us_layout);    
 		};	
 		
 		//Code that allows Google Analytics to work
